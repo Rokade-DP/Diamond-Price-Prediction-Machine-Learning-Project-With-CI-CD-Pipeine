3ccefbea148cbbca567864fad3825a4168ea8744
Dhananjay Rokade <dhananjayrokade9665@gmail.com>
2023-12-19 10:53:20 +0530

    Project Completed

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,7 +1,7 @@
-# basic Import
-import numpy as numpy
+# Basic Import
+import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
+from sklearn.linear_model import LinearRegression, Ridge,Lasso,ElasticNet
 from sklearn.tree import DecisionTreeRegressor
 from src.exception import CustomException
 from src.logger import logging
@@ -13,18 +13,19 @@ from dataclasses import dataclass
 import sys
 import os
 
-@dataclass
+@dataclass 
 class ModelTrainerConfig:
-    trained_model_file_path = os.path.join("artifacts","model.pkl")
+    trained_model_file_path = os.path.join('artifacts','model.pkl')
+
 
 class ModelTrainer:
     def __init__(self):
-        self.trained_model_file_path = os.path.join("artifacts","model.pkl")
-    
-    def initiate_model_training(self, train_array , test_array):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initate_model_training(self,train_array,test_array):
         try:
-            logging.info("Splitting Dependent and Independent Variable from train and test data")
-            X_train , y_train, X_test, y_test = (
+            logging.info('Splitting Dependent and Independent variables from train and test data')
+            X_train, y_train, X_test, y_test = (
                 train_array[:,:-1],
                 train_array[:,-1],
                 test_array[:,:-1],
@@ -32,31 +33,31 @@ class ModelTrainer:
             )
 
             models={
-                'LinearRegression':LinearRegression(),
-                'Lasso':Lasso(),
-                'Ridge':Ridge(),
-                'Elasticnet':ElasticNet(),
-                'DecisionTree':DecisionTreeRegressor()
-            }
-
-            model_report : dict = evaluate_model(X_train, y_train, X_test, y_test, models)
+            'LinearRegression':LinearRegression(),
+            'Lasso':Lasso(),
+            'Ridge':Ridge(),
+            'Elasticnet':ElasticNet(),
+            'DecisionTree':DecisionTreeRegressor()
+        }
+            
+            model_report:dict=evaluate_model(X_train,y_train,X_test,y_test,models)
             print(model_report)
-            print('\n=======================================================================\n')
-            logging.info(f'Model Report:{model_report}')
+            print('\n====================================================================================\n')
+            logging.info(f'Model Report : {model_report}')
 
-            #To get best model score from dictionary
-
+            # To get best model score from dictionary 
             best_model_score = max(sorted(model_report.values()))
 
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
-
+            
             best_model = models[best_model_name]
 
-            print(f'Best Model Found , Model Name : {best_model_name}, R2 Score : {best_model_score}')
-            print("\n==========================================================================================\n")
-            logging.info(f"Best Model Found , Model Name : {best_model_name},  R2 Score : {best_model_score}")
+            print(f'Best Model Found , Model Name : {best_model_name} , R2 Score : {best_model_score}')
+            print('\n====================================================================================\n')
+            logging.info(f'Best Model Found , Model Name : {best_model_name} , R2 Score : {best_model_score}')
+
             save_object(
                  file_path=self.model_trainer_config.trained_model_file_path,
                  obj=best_model
@@ -66,4 +67,3 @@ class ModelTrainer:
         except Exception as e:
             logging.info('Exception occured at Model Training')
             raise CustomException(e,sys)
-
